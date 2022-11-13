d821ad8ec2188f17c85b75c94807fb53bf2d96fc
Shakib <mdshakibal91@gmail.com>
2022-11-13 17:34:04 +0600

    new

---
+++ withoutModoparator.cpp
@@ -0,0 +1,10 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int a,b;
+    cin >> a >> b;
+        int vag = a/b;
+        int gun = vag*b;
+        int rem = a-gun;
+        cout << rem << "\n"; 
+}
