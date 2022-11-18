fcf629482030f4354a0c1ed5ba29cf2627a6992b
shakib <mdshakibal91@gmail.com>
2022-11-18 18:27:08 +0600

    patternN

---
+++ patternN.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int n;
+    cin >> n;
+    for(int row=1;row<=n;row++){
+        for(int col=1;col<=n-row;col++){
+            cout << " ";
+        }
+        for(int col=1;col<=2*row-1;col++){
+            cout << "*";
+        }
+        cout << "\n";
+    }
+
+}
