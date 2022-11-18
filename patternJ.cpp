2a927f3b1a0dd55cd493b9dd7cb5d3765d647995
shakib <mdshakibal91@gmail.com>
2022-11-18 18:22:35 +0600

    patternJ

---
+++ patternJ.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int n;
+    cin >> n;
+    for(int row=1;row<=n;row++){
+        for(int col=1;col<=n-row;col++){
+            cout << " ";
+        }
+        for(int col=1;col<=row;col++){
+            cout << "*";
+        }
+        cout << "\n";
+    }
+}
+
