83941c093e9affef19c6251327595312c56b9d7d
shakib <mdshakibal91@gmail.com>
2022-11-18 18:37:51 +0600

    patternQ

---
+++ patternQ.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+    for(int row=1;row<=n;row++){
+        for(int col=1;col<=n-row;col++){
+            cout<< " ";
+        }
+        for(int col=1;col<=row;col++){
+            cout << "*";
+        }
+        cout << "\n";
+    }
+    for(int row=n-1;row>=1;row--){
+        for(int col=1;col<=n-row;col++){
+            cout<< " ";
+        }
+        for(int col=1;col<=row;col++){
+            cout << "*";
+        }
+        cout << "\n";
+    }
+}
+
